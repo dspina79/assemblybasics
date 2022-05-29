8bcca1fb789b2882600a19b895d54c14115f3665
Dave Spina <dspina79@gmail.com>
2022-05-28 21:26:41 -0400

    First demo

---
+++ demo.s
@@ -0,0 +1,20 @@
+.export Main
+.segment "CODE"
+
+.proc Main
+    ; load 5 into each of the registers x and y
+    ldx #5
+    ldy #5
+
+    ; increment the value in x twice
+    inx
+    inx
+
+    ; decrement the value of x once
+    dex
+
+    ; increment y by 1
+    iny
+
+    rts 
+.endproc
